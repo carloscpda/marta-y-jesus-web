fa3c85281cf730ff7b266789ea0e5f259b8027b7
Carlos Cepeda <carlos@cepeda.me>
2022-08-17 20:17:29 +0200

    evita dobles clicks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 import { navigate } from "gatsby"
-import React, { useEffect } from "react"
+import React, { useState } from "react"
 import { Helmet } from "react-helmet"
 import { useForm } from "react-hook-form"
 import Switch from "../components/switch"
@@ -42,7 +42,10 @@ export default function Index() {
   const { register, handleSubmit, control, watch, setValue } =
     useForm<FormParams>()
 
+  const [isSubmitted, setIsSubmitted] = useState(false)
+
   const onSubmit = async (params: FormParams) => {
+    setIsSubmitted(true)
     await axios.post("/api/submission", params)
     navigate("thank-you")
   }
@@ -277,6 +280,7 @@ export default function Index() {
                 </div>
                 <div className="sm:col-span-2">
                   <button
+                    disabled={isSubmitted}
                     type="submit"
                     className="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                   >
